d880eeca7d9b87f74474de4f38ab6c6e2a0edd57
yash-agrwl <agarwalyash7012@gmail.com>
2022-03-23 01:18:04 +0530

    added empty project file with welcome message

---
+++ FindMaximumUsingGenerics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindMaximumUsingGenerics
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the Find Maximum Problem Using Generics.\n");
+        }
+    }
+}
