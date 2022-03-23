55888f8638b7fff693afd35d0fb706ceb6edf3da
yash-agrwl <agarwalyash7012@gmail.com>
2022-03-23 12:55:19 +0530

    added UC2: Ability to find the maximum of 3 Floats

--- FindMaximumUsingGenerics/MaximumNumberCheck.cs
+++ FindMaximumUsingGenerics/MaximumNumberCheck.cs
@@ -18,5 +18,16 @@ namespace FindMaximumUsingGenerics
 
             return thirdValue;
         }
+
+        public static double MaxFloatNumber(double firstValue, double secondValue, double thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) >= 0)
+                return firstValue;
+
+            if (secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) >= 0)
+                return secondValue;
+
+            return thirdValue;
+        }
     }
 }
--- FindMaximumUsingGenerics/Program.cs
+++ FindMaximumUsingGenerics/Program.cs
@@ -8,10 +8,13 @@ namespace FindMaximumUsingGenerics
         {
             Console.WriteLine("Welcome to the Find Maximum Problem Using Generics.\n");
 
-            Console.WriteLine("The Maximum Number is: ");
+            Console.WriteLine("The Maximum Numbers are: ");
 
-            int output = MaximumNumberCheck.MaxIntegerNumber(11, 33, 55);
-            Console.WriteLine(output);
+            int intMax = MaximumNumberCheck.MaxIntegerNumber(11, 33, 55);
+            Console.WriteLine(intMax);
+
+            double floatMax = MaximumNumberCheck.MaxFloatNumber(119.12, 33.34, 45.65);
+            Console.WriteLine(floatMax);
         }
     }
 }
