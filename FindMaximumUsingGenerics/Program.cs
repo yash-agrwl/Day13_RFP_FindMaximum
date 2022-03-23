a0a533d54935f57a879dca35b4f01877c1beedb5
yash-agrwl <agarwalyash7012@gmail.com>
2022-03-23 13:40:35 +0530

    moved all the methods to a single class containing main

--- FindMaximumUsingGenerics/Program.cs
+++ FindMaximumUsingGenerics/Program.cs
@@ -10,11 +10,33 @@ namespace FindMaximumUsingGenerics
 
             Console.WriteLine("The Maximum Numbers are: ");
 
-            int intMax = MaximumNumberCheck.MaxIntegerNumber(11, 33, 55);
+            int intMax = MaxIntegerNumber(11, 33, 55);
             Console.WriteLine(intMax);
 
-            double floatMax = MaximumNumberCheck.MaxFloatNumber(119.12, 33.34, 45.65);
+            double floatMax = MaxFloatNumber(119.12, 33.34, 45.65);
             Console.WriteLine(floatMax);
+        }
+
+        public static int MaxIntegerNumber(int firstValue, int secondValue, int thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) >= 0)
+                return firstValue;
+
+            if (secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) >= 0)
+                return secondValue;
+
+            return thirdValue;
+        }
+
+        public static double MaxFloatNumber(double firstValue, double secondValue, double thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) >= 0)
+                return firstValue;
+
+            if (secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) >= 0)
+                return secondValue;
+
+            return thirdValue;
         }
     }
 }
