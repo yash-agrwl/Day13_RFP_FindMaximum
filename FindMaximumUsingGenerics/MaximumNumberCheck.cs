57c3e8721e060414141f27344b92e3341d552570
yash-agrwl <agarwalyash7012@gmail.com>
2022-03-23 12:46:07 +0530

    added UC1: Ability to find the maximum of 3 Integers

---
+++ FindMaximumUsingGenerics/MaximumNumberCheck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumUsingGenerics
+{
+    internal class MaximumNumberCheck
+    {
+        public static int MaxIntegerNumber(int firstValue, int secondValue, int thirdValue)
+        {
+            if( firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) >=0 )
+                return firstValue;
+
+            if( secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) >= 0 )
+                return secondValue;
+
+            return thirdValue;
+        }
+    }
+}
--- FindMaximumUsingGenerics/Program.cs
+++ FindMaximumUsingGenerics/Program.cs
@@ -7,6 +7,11 @@ namespace FindMaximumUsingGenerics
         static void Main()
         {
             Console.WriteLine("Welcome to the Find Maximum Problem Using Generics.\n");
+
+            Console.WriteLine("The Maximum Number is: ");
+
+            int output = MaximumNumberCheck.MaxIntegerNumber(11, 33, 55);
+            Console.WriteLine(output);
         }
     }
 }
