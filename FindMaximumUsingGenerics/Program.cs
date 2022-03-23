b1eaae6fce20e5f4ef1b0264df02e039d10cda73
yash-agrwl <agarwalyash7012@gmail.com>
2022-03-23 13:46:46 +0530

    added Refactor1: Refactored all the 3 methods to 1 Generic Method to find the maximum

--- FindMaximumUsingGenerics/Program.cs
+++ FindMaximumUsingGenerics/Program.cs
@@ -10,39 +10,17 @@ namespace FindMaximumUsingGenerics
 
             Console.WriteLine("The Maximum Numbers are: ");
 
-            int intMax = MaxIntegerNumber(11, 33, 55);
+            int intMax = MaxValue<int>(11, 33, 55);
             Console.WriteLine(intMax);
 
-            double floatMax = MaxFloatNumber(119.12, 33.34, 45.65);
+            double floatMax = MaxValue<double>(119.12, 33.34, 45.65);
             Console.WriteLine(floatMax);
 
-            string stringMax = MaxString("asad", "aakash", "adarsh");
+            string stringMax = MaxValue<string>("asad", "aakash", "adarsh");
             Console.WriteLine(stringMax);
         }
 
-        public static int MaxIntegerNumber(int firstValue, int secondValue, int thirdValue)
-        {
-            if (firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) >= 0)
-                return firstValue;
-
-            if (secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) >= 0)
-                return secondValue;
-
-            return thirdValue;
-        }
-
-        public static double MaxFloatNumber(double firstValue, double secondValue, double thirdValue)
-        {
-            if (firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) >= 0)
-                return firstValue;
-
-            if (secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) >= 0)
-                return secondValue;
-
-            return thirdValue;
-        }
-
-        public static string MaxString(string firstValue, string secondValue, string thirdValue)
+        public static T MaxValue<T>(T firstValue, T secondValue, T thirdValue) where T : IComparable
         {
             if (firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) >= 0)
                 return firstValue;
